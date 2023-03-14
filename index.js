c0020b3ee26ed0d6fc19dbb6c1f3f210d93a1cd1
boyraf <raphael.kymotho@gmail.com>
2023-03-14 12:50:32 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
-
-// Write your code here
+let num1 =2;
+let num2 =31;
+let multiply = num1 * num2;
+let random = Number.isInteger(Math.random() *100) + 1;
+let num3 =16;
+let num4 =6
+let mod = num3 % num4;
+let max =Math.max(1,3,7,9,13,16,18,20)
